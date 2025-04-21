30aad5d50ac422c0e81d1109d376e583538cbae0
Navnath3864 <nagutte0@gmail.com>
2025-04-21 16:37:52 +0530

    add spcace demo commit

---
+++ EnquiryForm/src/main/java/com/app/EnquiryFormApplication.java
@@ -0,0 +1,23 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EnquiryFormApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EnquiryFormApplication.class, args);
+		
+	}
+	 
+	@Bean
+	//@LoadBalanced
+	public RestTemplate rt() 
+	{
+		return new RestTemplate();
+			
+	}
+}
